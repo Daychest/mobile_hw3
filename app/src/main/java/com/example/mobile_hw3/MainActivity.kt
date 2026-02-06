010461d18196f585f77ac49e589c55da46665653
Daychest <veikka.paivarinta@gmail.com>
2026-02-06 12:40:26 +0200

    Async image not showing anything

--- app/src/main/java/com/example/mobile_hw3/MainActivity.kt
+++ app/src/main/java/com/example/mobile_hw3/MainActivity.kt
@@ -6,12 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import com.example.mobile_hw3.ui.theme.Mobile_hw3Theme
 
 class MainActivity : ComponentActivity() {
@@ -34,13 +42,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     AsyncImage(
-        model = "https://example.com/image.jpg",
-        contentDescription = null,
+        model = ImageRequest.Builder(LocalContext.current)
+            .data("https://example.com/image.jpg")
+            .crossfade(true)
+            .build(),
+        placeholder = painterResource(R.drawable.reaction),
+        contentDescription = stringResource(R.string.app_name),
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.clip(CircleShape),
     )
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
 }
 
 @Preview(showBackground = true)
