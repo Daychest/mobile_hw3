197dcf04523724f70d06d962411e2e7c42858b9b
Daychest <veikka.paivarinta@gmail.com>
2026-02-06 22:07:56 +0200

    added roomdb and viewmodel

---
+++ app/src/main/java/com/example/mobile_hw3/viewModel/NoteViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.test.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.test.roomDb.Note
+import kotlinx.coroutines.launch
+
+class NoteViewModel(private val repository: Repository): ViewModel() {
+    fun getNotes() = repository.getAllNotes().asLiveData(viewModelScope.coroutineContext)
+
+    fun upsertNote(note: Note){
+        viewModelScope.launch {
+            repository.upsertNote(note)
+        }
+    }
+    fun deleteNote(note: Note){
+        viewModelScope.launch {
+            repository.deleteNote(note)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mobile_hw3/viewModel/Repository.kt
@@ -0,0 +1,20 @@
+package com.example.test.viewModel
+
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.RoomDatabase
+import androidx.room.Upsert
+import com.example.test.roomDb.Note
+import com.example.test.roomDb.NoteDatabase
+
+class Repository(private  val db: NoteDatabase) {
+    suspend fun upsertNote(note: Note){
+        db.dao.upsertNote(note)
+    }
+
+    suspend fun deleteNote(note: Note){
+        db.dao.deleteNote(note)
+    }
+
+    fun getAllNotes() = db.dao.getAllNotes()
+}
