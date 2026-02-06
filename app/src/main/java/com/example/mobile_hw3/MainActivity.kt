1ec7624fb025642cfa72d41375cbdc04269193d8
Daychest <veikka.paivarinta@gmail.com>
2026-02-06 13:48:40 +0200

    added navigation stuff from hw2

--- app/src/main/java/com/example/mobile_hw3/MainActivity.kt
+++ app/src/main/java/com/example/mobile_hw3/MainActivity.kt
@@ -26,17 +26,36 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobile_hw3.ui.theme.Mobile_hw3Theme
 import android.net.Uri
 import androidx.activity.result.PickVisualMediaRequest
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 
@@ -46,15 +65,204 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Mobile_hw3Theme {
-                MainScreen()
+                Test()
             }
+        }
+    }
+}
+
+const val mainScreenRoute = "mainScreen"
+const val messageScreenRoute = "messageScreen"
+const val settingsScreenRoute = "settingsScreen"
+
+@Preview
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = mainScreenRoute) {
+        composable(route = mainScreenRoute) {
+            MainScreen(navController = navController)
+        }
+        composable(route = messageScreenRoute) {
+            MessageScreen(navController = navController)
+        }
+        composable(route = settingsScreenRoute) {
+            SettingsScreen(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 50.dp)
+    ) {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp),
+            onClick = {
+                navController.navigate(messageScreenRoute)
+            },
+        ) {
+            Text(text = "Strange Man")
+        }
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                navController.navigate(settingsScreenRoute)
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)
+        ) {
+            Text(text = "Settings")
+        }
+    }
+}
+
+
+@Composable
+fun MessageScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(
+                modifier = Modifier.width(190.dp),
+                onClick = {
+                    navController.popBackStack()
+                },
+            ) {
+                Text(text = "Back")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(
+                modifier = Modifier.width(190.dp),
+                onClick = {
+                    navController.navigate(settingsScreenRoute)
+                },
+                colors = ButtonDefaults.buttonColors(Color.DarkGray)
+            ) {
+                Text(text = "Settings")
+            }
+        }
+        Mobile_hw3Theme() {
+            Conversation(SampleData.conversationSample)
+        }
+    }
+}
+
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 50.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "No Settings",
+            fontSize = 40.sp,
+        )
+        Button(
+            onClick = {
+                navController.popBackStack()
+            },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(),
+        ) {
+            Text(text = "Back")
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun ProfilePicture(picture: Int) {
+    Image(
+        painter = painterResource(picture),
+        contentDescription = null,
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+    )
+}
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        ProfilePicture(R.drawable.reaction)
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
         }
     }
 }
 
 @Preview
 @Composable
-fun MainScreen() {
+fun Test() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
